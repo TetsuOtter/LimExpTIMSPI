25926ce88be0511854f9fa6ac23d36b75e8c8e53
ＴＲ <public@technotter.com>
2019-04-23 12:39:16 +0900

    Set the Loading PI Name

--- LimExpTIMS/AskATSPsP.cs
+++ LimExpTIMS/AskATSPsP.cs
@@ -13,37 +13,37 @@ namespace TR.LimExpTIMS
     //Refer : https://tomosoft.jp/design/?p=4607
 
     public const CallingConvention CalCnv = CallingConvention.StdCall;
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void Load();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void Dispose();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void SetVehicleSpec();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void Initialize();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void Elapse();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void SetPower();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void SetBrake();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void SetReverser();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void KeyDown();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void KeyUp();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void HornBlow();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void DoorOpen();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void DoorClose();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void SetSignal();
-    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    [DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     internal static extern void SetBeaconData();
-    //[DllImport("ATS.dll", CallingConvention = CalCnv)]
+    //[DllImport("Integrated_ATS(Ps).dll", CallingConvention = CalCnv)]
     //internal static extern void GetPluginVersion();
 
   }
