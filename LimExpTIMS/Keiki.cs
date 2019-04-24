58866cc05315ffe6bd3fce7d4cf2946bac98b962
ＴＲ <public@technotter.com>
2019-04-24 23:02:29 +0900

    車両基本機能の一部を実装

--- LimExpTIMS/Keiki.cs
+++ LimExpTIMS/Keiki.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace TR.LimExpTIMS
 {
-  class Keiki
+  static class Keiki
   {
     //No.1(実装準備) / No.1+2 / No.2計器モニタ表示
+    static class No1
+    {
+
+    }
+    static internal class No12
+    {
+      internal static unsafe void Panel(int* Pa)
+      {
+        Pa[21] = Main.NFBConfig.SnoBrake ? 1 : 0;
+      }
+    }
+    static class No2
+    {
+      //特に独自処理はない。
+    }
   }
 }
--- LimExpTIMS/Main.cs
+++ LimExpTIMS/Main.cs
@@ -19,7 +19,31 @@ namespace TR.LimExpTIMS
     //IC R/W関連
     public static bool ICReaded = false;
     public static bool ICReading = false;
-    
+
+    //操作音関連
+    /// <summary>TIMS Touching Sound Play TF</summary>
+    internal static bool IsTIMSTouching = false;
+    /// <summary>NFB Switch Sound Play TF</summary>
+    private static bool IsNFBSwitching = false;
+    private static bool IsButtonPushed = false;
+    static private bool IsButtonPushing = false;
+    private static bool IsCabSeSMovingToCenter = false;
+    private static bool IsCabSeSMovingToEdge = false;
+
+    //MCKey関連
+    private static bool IsMCKeyToON = false;
+    private static bool IsMCKeyToOFF = false;
+    private static bool IsMCKeyInserted = false;
+    private static bool IsMCKeyRemoved = false;
+    private static bool IsMCKeyTurnFailed = false;
+
+    //停車前デッキ放送
+    private static bool IsBeforeStoppingAnnouncePlayed = false;
+
+    //IC関連
+    private static bool IsICInserted = false;
+    private static bool IsICRemoved = false;
+
     //バックライト関連
     private static int No1KeikiBL = BLBlack;
     private static int No2KeikiBL = BLBlack;
@@ -34,7 +58,7 @@ namespace TR.LimExpTIMS
     private static bool IsTIMSSoundPushed = false;
 
     //TIMS PageNumber
-    private enum TIMSPageNum//あとで適宜変更
+    private enum TIMSPageENum//あとで適宜変更
     {
       None,
       S00AA,
@@ -45,6 +69,40 @@ namespace TR.LimExpTIMS
       D05AA,
 
     };
+    static TIMSPageENum TIMSPageNum = TIMSPageENum.S00AB;
+
+    private enum CabSeSLocationENum { F, N, B };
+    static CabSeSLocationENum CabSeSLoc = CabSeSLocationENum.N;
+
+    private enum DisplayingModeENum
+    {
+      Driving,
+      CabNFBShowing,
+      CabSeSShowing,
+      OutOfCar
+    };
+    private static DisplayingModeENum DispMode = DisplayingModeENum.Driving;
+
+    private static MCKeyStateENum MCKeyState = MCKeyStateENum.Removed;
+    private enum MCKeyStateENum
+    {
+      Removed,
+      OFF,
+      ON
+    };
+
+
+    static internal bool Turn(in this bool b) => !b;//BOOLを反転させる
+
+    /// <summary>Sound Play Once Method</summary>
+    /// <param name="So">Sound Output State Value</param>
+    /// <param name="PlayingState">Playing State</param>
+    static private void SPO(ref this int So, ref bool PlayingState)
+    {
+      if (PlayingState) { So = Sound.Once; PlayingState = false; }
+      else So = Sound.Continue;
+    }
+
 
     static internal void Load()
     {
@@ -134,7 +192,6 @@ namespace TR.LimExpTIMS
       }
 
     }
-
     static internal void Dispose()
     {
       if (TRBIDSppLoaded) TRBIDSpp.Dispose();
@@ -142,7 +199,6 @@ namespace TR.LimExpTIMS
       //if(KikuSC59ALoaded)KikuSC59A.Dispose();
       //if(KikuTIMSLoaded)KikuTIMS.Dispose();
     }
-
     static internal void SetVehicleSpec(Spec s)
     {
       if (TRBIDSppLoaded) TRBIDSpp.SetVehicleSpec(s);
@@ -150,13 +206,29 @@ namespace TR.LimExpTIMS
       if (KikuSC59ALoaded) KikuSC59A.SetVehicleSpec(s);
       if (KikuTIMSLoaded) KikuTIMS.SetVehicleSpec(s);
     }
-
     static internal void Initialize(int s)
     {
       if (TRBIDSppLoaded) TRBIDSpp.Initialize(s);
       if (AskATSPsPLoaded) AskATSPsP.Initialize(s);
       if (KikuSC59ALoaded) KikuSC59A.Initialize(s);
       if (KikuTIMSLoaded) KikuTIMS.Initialize(s);
+
+      switch (s)//CabSeS位置選定
+      {
+        case InitialPos.Emergency:
+          CabSeSLoc = CabSeSLocationENum.F;
+          MCKeyState = MCKeyStateENum.Removed;
+          break;
+        case InitialPos.Removed:
+          CabSeSLoc = CabSeSLocationENum.N;
+          MCKeyState = MCKeyStateENum.Removed;
+          break;
+        case InitialPos.Service:
+          CabSeSLoc = CabSeSLocationENum.F;
+          MCKeyState = MCKeyStateENum.ON;
+          break;
+      }
+
     }
     //Attention! This Method uses "UNSAFE".
     static unsafe internal void Elapse(State st, int* Pa, int* Sa)
@@ -167,9 +239,25 @@ namespace TR.LimExpTIMS
       if (KikuSC59ALoaded) Ats.Handle = KikuSC59A.Elapse(st, Pa, Sa);
       if (KikuTIMSLoaded) Ats.Handle = KikuTIMS.Elapse(st, Pa, Sa);
 
-      BLSoSet(Pa);
-    }
 
+      //BackLight / Sound Btn Displaying
+      BLSoSet(Pa);
+
+      DrivingPanels(Pa);
+      CabNFBPanels(Pa);
+      CabSeSChangingPanel(Pa);
+      OutOfCarPanel(Pa);
+
+      OnceSoundSetting(Sa);
+
+      Keiki.No12.Panel(Pa);
+
+      //HELP1
+      if (Ats.IsKeyDown[ATSKeys.S]) Pa[250] = 1 + (int)DispMode;
+      else Pa[250] = 0;
+
+      
+    }
     static internal void SetPower(int p)
     {
       if (TRBIDSppLoaded) TRBIDSpp.SetPower(p);
@@ -177,7 +265,6 @@ namespace TR.LimExpTIMS
       if (KikuSC59ALoaded) KikuSC59A.SetPower(p);
       if (KikuTIMSLoaded) KikuTIMS.SetPower(p);
     }
-
     static internal void SetBrake(int b)
     {
       if (TRBIDSppLoaded) TRBIDSpp.SetBrake(b);
@@ -185,7 +272,6 @@ namespace TR.LimExpTIMS
       if (KikuSC59ALoaded) KikuSC59A.SetBrake(b);
       if (KikuTIMSLoaded) KikuTIMS.SetBrake(b);
     }
-
     static internal void SetReverser(int r)
     {
       if (TRBIDSppLoaded) TRBIDSpp.SetReverser(r);
@@ -201,7 +287,7 @@ namespace TR.LimExpTIMS
       //if(KikuTIMSLoaded) KikuTIMS.KeyDown(k);
 
 
-      //BL and Sound Keys Setting Start
+      //BL and Sound Keys Setting
       if (Ats.IsKeyDown[0])
       {
         switch (k)
@@ -211,10 +297,7 @@ namespace TR.LimExpTIMS
             IsNo1BLKeyPushed = true;
             break;
           case 8://TIMSSo
-            if (!IsTIMSAngry && !IsTIMSSoundPushed)
-            {
-              BLSoChangeEvent(4);
-            }
+            if (!IsTIMSAngry && !IsTIMSSoundPushed) BLSoChangeEvent(4);
             IsTIMSSoundPushed = true;
             break;
           case 9://TIMSBL
@@ -227,18 +310,105 @@ namespace TR.LimExpTIMS
             break;
         }
       }
-      //BL and Sound Keys Setting End
-
+      else
+      {
+        switch (DispMode)
+        {
+          case DisplayingModeENum.Driving:
+            break;
+          case DisplayingModeENum.CabNFBShowing:
+            CabNFBModeKeyEvent(k);
+            break;
+          case DisplayingModeENum.CabSeSShowing:
+            break;
+          case DisplayingModeENum.OutOfCar:
+            break;
+        }
+      }
     }
-
     static internal void KeyUp(int k)
     {
       if (TRBIDSppLoaded) TRBIDSpp.KeyUp(k);
       if (AskATSPsPLoaded) AskATSPsP.KeyUp(k);
       //if(KikuSC59ALoaded) KikuSC59A.KeyUp(k);
       //if(KikuTIMSLoaded) KikuTIMS.KeyUp(k);
-    }
 
+      //BL and Sound Keys Setting Start
+      switch (k)
+      {
+        case 7://No1
+          IsNo1BLKeyPushed = false;
+          break;
+        case 8://TIMSSo
+          IsTIMSSoundPushed = false;
+          break;
+        case 9://TIMSBL
+          IsTIMSBLKeyPushed = false;
+          break;
+        case 10://No2
+          IsNo2BLKeyPushed = false;
+          break;
+      }
+      //BL and Sound Keys Setting End
+
+      //Mode Changing Event
+      if (Ats.IsKeyDown[0])
+      {
+        //Num Up
+        if (k == 5)
+        {
+          switch (DispMode)
+          {
+            case DisplayingModeENum.CabNFBShowing:
+              DispMode = DisplayingModeENum.Driving;
+              break;
+            case DisplayingModeENum.CabSeSShowing:
+              DispMode = DisplayingModeENum.CabNFBShowing;
+              break;
+            case DisplayingModeENum.OutOfCar:
+              DispMode = DisplayingModeENum.CabSeSShowing;
+              break;
+          }
+        }
+        //Num Down
+        if (k == 6)
+        {
+          switch (DispMode)
+          {
+            case DisplayingModeENum.Driving:
+              DispMode = DisplayingModeENum.CabNFBShowing;
+              break;
+            case DisplayingModeENum.CabNFBShowing:
+              DispMode = DisplayingModeENum.CabSeSShowing;
+              break;
+            case DisplayingModeENum.CabSeSShowing:
+              DispMode = DisplayingModeENum.OutOfCar;
+              break;
+          }
+        }
+        MCKeyTurningEvent(k);
+      }
+      else
+      {
+        switch (DispMode)
+        {
+          case DisplayingModeENum.Driving:
+            break;
+          case DisplayingModeENum.CabNFBShowing:
+            if (k == 12)
+            {
+              NFBConfig.PantaRaise = false;
+              IsButtonPushed = true;
+            }
+            break;
+          case DisplayingModeENum.CabSeSShowing:
+            CabSeSTurningEvent(k);
+            break;
+          case DisplayingModeENum.OutOfCar:
+            break;
+        }
+      }
+    }
     static internal void DoorOpen()
     {
       if (TRBIDSppLoaded) TRBIDSpp.DoorOpen();
@@ -276,9 +446,230 @@ namespace TR.LimExpTIMS
     }
 
 
+    static private unsafe void OnceSoundSetting(int* Sa)
+    {
+      //TIMS Touching Sound
+      Sa[111].SPO(ref IsTIMSTouching );
+
+      //CabSeS Turning Sound
+      Sa[93].SPO(ref IsCabSeSMovingToCenter );
+      Sa[94].SPO(ref IsCabSeSMovingToEdge );
+
+      //NFB Switching Sound
+      Sa[102].SPO(ref IsNFBSwitching );
+
+      //Button Pushing/Pushed Sound
+      Sa[115].SPO(ref IsButtonPushing );
+      Sa[116].SPO(ref IsButtonPushed );
+
+      //MCKeys
+      Sa[89].SPO(ref IsMCKeyRemoved);
+      Sa[90].SPO(ref IsMCKeyToON);
+      Sa[91].SPO(ref IsMCKeyToOFF);
+      Sa[92].SPO(ref IsMCKeyInserted);
+    }
+
+    static bool CabNFBModeOffed = false;
+    static bool IsNFBTurned = false;
+    
+    internal static class NFBConfig
+    {
+      //true:入, false:auto
+      static public bool TailLamp { get=> _TailLamp; set { _TailLamp = value; IsNFBTurned = true; } }
+      static private bool _TailLamp = false;
+
+      //true:増, false:通常
+      static public bool CurrentLimUp { get => _CurrentLimUp; set { _CurrentLimUp = value; IsNFBTurned = true; } }
+      static private bool _CurrentLimUp = false;
+
+      //true:Enabled, false:Disabled
+      static public bool ElectricB { get => _ElectricB; set { _ElectricB = value; IsNFBTurned = true; } }
+      static private bool _ElectricB = false;
 
 
-    static private unsafe void BLSoSet(int* Pa)//
+      //same as above
+      static public bool SnoBrake { get => _SnoBrake; set { _SnoBrake = value; IsNFBTurned = true; } }
+      static private bool _SnoBrake = false;
+
+      //true:Break, false:Linked
+      static public bool DoorLinkBreak { get => _DoorLinkBreak; set { _DoorLinkBreak = value; IsNFBTurned = true; } }
+      static private bool _DoorLinkBreak = false;
+
+      //true:Pushed, false:Released
+      static public bool PantaRaise { get => _PantaRaise; set { _PantaRaise = value; IsNFBTurned = true; } }
+      static private bool _PantaRaise = false;
+
+    }
+
+    static private unsafe void CabNFBPanels(int* Pa)
+    {
+      const int BaseIndex = 212;
+      const int NFB1Index = 213;//Assign33
+      const int NFB2Index = 214;//Assign34
+      const int NFB3Index = 215;//Assign35
+      if (DispMode != DisplayingModeENum.CabNFBShowing)
+      {
+        Pa[BaseIndex] = 0;
+        Pa[NFB1Index] = 0;
+        Pa[NFB2Index] = 0;
+        Pa[NFB3Index] = 0;
+        CabNFBModeOffed = true;
+      }
+      else if (CabNFBModeOffed || IsNFBTurned)
+      {
+        Pa[BaseIndex] = 1;
+        Pa[NFB1Index] = (NFBConfig.TailLamp ? 4 : 0) + (NFBConfig.CurrentLimUp ? 2 : 0) + (NFBConfig.ElectricB ? 1 : 0);
+        Pa[NFB2Index] = (NFBConfig.SnoBrake ? 4 : 0) + (NFBConfig.DoorLinkBreak ? 2 : 0) + (NFBConfig.PantaRaise ? 1 : 0);
+        Pa[NFB3Index] = 1;
+        CabNFBModeOffed = false;
+        IsNFBTurned = false;
+      }
+    }
+    static private void CabNFBModeKeyEvent(int k)
+    {
+      bool IsPushSW = false;
+      switch (k)
+      {
+        case 7:
+          NFBConfig.TailLamp.Turn();
+          break;
+        case 8:
+          NFBConfig.CurrentLimUp.Turn();
+          break;
+        case 9:
+          NFBConfig.ElectricB.Turn();
+          break;
+        case 10:
+          NFBConfig.SnoBrake.Turn();
+          break;
+        case 11:
+          NFBConfig.DoorLinkBreak.Turn();
+          break;
+        case 12:
+          IsPushSW = true;
+          NFBConfig.PantaRaise = true;
+          break;
+        case 5:
+          //VCB ON (予約)
+          break;
+        case 6:
+          //VCB OFF (予約)
+          break;
+      }
+      if (IsPushSW) IsButtonPushing = true;
+      else IsNFBSwitching = true;
+    }
+
+
+    static private unsafe void DrivingPanels(int* Pa)
+    {
+      if (DispMode == DisplayingModeENum.Driving)
+      {
+
+      }
+      else
+      {
+
+      }
+    }
+
+    static private unsafe void CabSeSChangingPanel(int* Pa) => Pa[28] = DispMode == DisplayingModeENum.CabSeSShowing ? 1 + (int)CabSeSLoc : 0;
+    static private void CabSeSTurningEvent(int k)
+    {
+      if (k == ATSKeys.C1)
+      {
+        switch (CabSeSLoc)
+        {
+          case CabSeSLocationENum.N:
+            CabSeSLoc = CabSeSLocationENum.F;
+            IsCabSeSMovingToEdge = true;
+            break;
+          case CabSeSLocationENum.B:
+            CabSeSLoc = CabSeSLocationENum.N;
+            IsCabSeSMovingToCenter = true;
+            break;
+        }
+      }
+      if (k == ATSKeys.C2)
+      {
+        switch (CabSeSLoc)
+        {
+          case CabSeSLocationENum.F:
+            CabSeSLoc = CabSeSLocationENum.N;
+            IsCabSeSMovingToCenter = true;
+            break;
+          case CabSeSLocationENum.N:
+            CabSeSLoc = CabSeSLocationENum.B;
+            IsCabSeSMovingToEdge = true;
+            break;
+        }
+      }
+    }
+
+    static private unsafe void OutOfCarPanel(int* Pa)
+    {
+      int outnum = 0;
+      if (DispMode == DisplayingModeENum.OutOfCar)
+      {
+        switch (CabSeSLoc)
+        {
+          case CabSeSLocationENum.F:
+            outnum = NFBConfig.TailLamp ? 3 : 1;
+            break;
+          default:
+            outnum = 2;
+            break;
+        }
+      }
+      Pa[240] = outnum;
+    }
+
+    static private unsafe void MCKeyTurningEvent(int k)
+    {
+      switch (MCKeyState)
+      {
+        case MCKeyStateENum.ON:
+          if (Ats.Handle.R == 0 && Ats.Handle.P == 0 && Ats.Handle.B == 0 && k == ATSKeys.I)
+          {
+            IsMCKeyToOFF = true;
+            MCKeyState = MCKeyStateENum.OFF;
+          }
+          else
+          {
+            IsMCKeyTurnFailed = true;
+          }
+          break;
+        case MCKeyStateENum.OFF:
+          if (k == ATSKeys.I)
+          {
+            IsMCKeyToON = true;
+            MCKeyState = MCKeyStateENum.ON;
+          }else if (k == ATSKeys.H)
+          {
+            IsMCKeyRemoved = true;
+            MCKeyState = MCKeyStateENum.Removed;
+          }
+          else
+          {
+            IsMCKeyTurnFailed = true;
+          }
+          break;
+        case MCKeyStateENum.Removed:
+          if (k == ATSKeys.H)
+          {
+            IsMCKeyInserted = true;
+            MCKeyState = MCKeyStateENum.OFF;
+          }
+          else
+          {
+            IsMCKeyTurnFailed = true;
+          }
+          break;
+      }
+    }
+
+
+    static private unsafe void BLSoSet(int* Pa)
     {
       //Display
       Pa[241] = No1KeikiBL;
