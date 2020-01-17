d8b5a47d575785ed9eea64d5d6fd7c2741fddbf9
ＴＲ <public@technotter.com>
2020-01-17 22:08:34 +0900

    Add Opt Page to DispProj

---
+++ LimExpTIMSDispW/CarsImage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TR.LimExpTIMSDisp
+{
+  public class CarsImage
+  {
+    static public List<CarInfo> ImgList;
+
+    public struct CarInfo
+    {
+      public bool IsEnabled;
+
+      public string CarNumber;
+
+      public PantographInfo LPan;
+      public bool IsLPanRaised;
+
+      public PantographInfo RPan;
+      public bool IsRPanRaised;
+
+      public bool IsL_Motored;
+      public bool IsR_Motored;
+
+      public bool IsDoubleDeck;
+      public bool IsLController;
+      public bool IsRController;
+
+      public Colors RGB;
+
+      public bool IsDoorAvailable;
+
+
+      public enum PantographInfo
+      {
+        None, Greater, Less, Diamond
+      }
+    }
+  }
+}
---
+++ LimExpTIMSDispW/Options/O02AA.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TR.LimExpTIMSDisp.Options
+{
+  /// <summary>
+  /// O02AA.xaml の相互作用ロジック
+  /// </summary>
+  public partial class O02AA : Page
+  {
+    public O02AA()
+    {
+      InitializeComponent();
+    }
+
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Page_Unloaded(object sender, RoutedEventArgs e)
+    {
+
+    }
+  }
+}
