62028cdcaff65460bcb748d69eb82c07733979ec
ＴＲ <public@technotter.com>
2020-04-27 01:42:50 +0900

    Update TimetableManager.cs

--- LimExpTIMS/Manager/TimetableManager.cs
+++ LimExpTIMS/Manager/TimetableManager.cs
@@ -9,12 +9,22 @@ namespace TR.LimExpTIMS
   /// <summary>時刻表を管理します.</summary>
   static public class TimetableManager
   {
+    static public Pnl_TIMSMon_TrainNum_Header TrNumHeader { get; private set; }
+    static public int TrNum { get; private set; }
+    static public Pnl_TIMSMon_TrainNum_Footer TrNumFooter { get; private set; }
+
     static public List<TIMS.StaInfo> StaList { get; private set; }
 
     /// <summary>TTMngerを初期化します.</summary>
     static public void Init()
     {
+      //イベントの購読を行う
+      Ats.SetBeaconEv += Ats_SetBeaconEv;
+    }
 
+    private static void Ats_SetBeaconEv(object sender, BeaconEvArgs e)
+    {
+      throw new NotImplementedException();
     }
   }
 }
